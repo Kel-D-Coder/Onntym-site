d24a2b7e0349a24e1d1d1ddb395cd5e5583fc63f
Kel-D-Coder <okolochibundu887@gmail.com>
2023-12-22 10:05:10 +0100

    fixed the bug in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
 import { NavLink, Link } from "react-router-dom"
 import '../styles/Navbar.css'
-// import logo from '../imgs/logo1.jpg'
+import { FaBars } from 'react-icons/fa6'
+import { useState } from "react"
+import 'animate.css'
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
+
+    function openNav(){
+        setOpen(!open)
+    }
+    
     return (
-        <nav>
-            <div className="navContainer">
+        <>
+            <div className="navContainer normal_nav">
                 <div className="logo">
                     <Link to={'/'}>
                         <h2 className="logo-text">AWVIX</h2>
@@ -19,7 +27,32 @@ const Navbar = () => {
                 </div>
             </div>
             <hr />
-        </nav>
+
+            <div className="responsive_nav">
+                <div className="responsive_logo">
+                    <Link to={'/'}>
+                        <h2 className="logo-text">AWVIX</h2>
+                    </Link>
+                    <div className="ham_menu">
+                        <button className="menu_btn" onClick={openNav}><FaBars /></button>
+                        <h1 className="menu_text">Menu</h1>
+                    </div>
+                </div>
+                
+                {open ? <div className={`responsive_links animate__animated animate__slideInDown`}>
+                        <NavLink to={'/'}>Home</NavLink>
+                        <NavLink to={'/about'}>About</NavLink>
+                        <NavLink to={'/menu'}>Menu</NavLink>
+                        <NavLink to={'/contact'}>Contact</NavLink>
+                    </div> : <div className={`responsive_links  animate__animated animate__slideOutUp`}>
+                        <NavLink to={'/'}>Home</NavLink>
+                        <NavLink to={'/about'}>About</NavLink>
+                        <NavLink to={'/menu'}>Menu</NavLink>
+                        <NavLink to={'/contact'}>Contact</NavLink>
+                    </div>
+                }
+            </div>
+        </>
     )
 }
 
